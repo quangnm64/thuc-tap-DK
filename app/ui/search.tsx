2f7ebb8b4ae116e920a0875621697caa33b8965b
quangnm64 <quang.nm.64cntt@ntu.edu.vn>
2025-12-02 09:46:30 +0700

    finished chapter 11

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -12,13 +12,14 @@ export default function Search({ placeholder }: { placeholder: string }) {
     console.log(`Searching... ${term}`);
   
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }, 1000);
+  }, 2000);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
